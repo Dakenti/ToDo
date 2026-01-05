0c113802b1f486fd06e665941d43643806e3de81
Dakenti <didar3@mail.ru>
2026-01-05 14:58:59 +0500

    - initialized xcode project, removed Main.Storyboard

---
+++ ToDo/ToDo/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  ToDo
+//
+//  Created by darkhan on 05.01.2026.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        return true
+    }
+}
---
+++ ToDo/ToDo/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by darkhan on 05.01.2026.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
